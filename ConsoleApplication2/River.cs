50f875c57e220d778c17c80b0d3826d07c8aaba0
gugusz <mr.gugusz@gmail.com>
2017-03-14 18:35:50 +0100

    initial commit

---
+++ ConsoleApplication2/River.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class River
+    {
+        public static int CountAliveFishes(int[] fishWeight, int[] fishDirection)
+        {
+            if (fishWeight.Length != fishDirection.Length)
+                throw new ArgumentException("arrays have different lenghts");
+
+            int aliveCounter = 0;
+            Stack<int> upstreamSwimmers = new Stack<int>();
+            for(int i=0; i<fishWeight.Length; i++)
+            {
+                if(fishDirection[i] == 0)
+                {
+                    if( EatUpstreamers(fishWeight[i],ref upstreamSwimmers))
+                    {
+                        aliveCounter++;
+                    }
+                }
+                else
+                {
+                    upstreamSwimmers.Push(fishWeight[i]);
+                }
+            }
+
+            aliveCounter += upstreamSwimmers.Count;
+
+            return aliveCounter;
+        }
+
+        public static bool EatUpstreamers(int eaterWeight,ref Stack<int> upstreamSwimmers)
+        {
+            List<int> swimmersList = upstreamSwimmers.ToList();
+            foreach (int upstreamer in swimmersList)
+            {
+                if(upstreamer < eaterWeight)
+                {
+                    upstreamSwimmers.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ConsoleApplicationTests/RiverUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using ConsoleApplication2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ConsoleApplicationTests
+{
+    [TestClass]
+    public class RiverUnitTest
+    {
+        [TestMethod]
+        public void Test1CountAlive()
+        {
+            int[] FishSizes = { 4, 3, 2, 1, 5 }, FishDirections = { 0, 1, 0, 0, 0 };
+            Assert.AreEqual(2, River.CountAliveFishes(FishSizes, FishDirections));
+        }
+
+        [TestMethod]
+        public void Test2CountAlive()
+        {
+            int[] FishSizes = { 4, 2, 3, 1, 5 }, FishDirections = { 0, 1, 0, 1, 0 };
+            Assert.AreEqual(3, River.CountAliveFishes(FishSizes, FishDirections));
+        }
+        
+        [TestMethod]
+        public void Test3CountAlive()
+        {
+            int[] FishSizes = { 4, 2, 3, 1, 5 }, FishDirections = { 0, 0, 0, 0, 0 };
+            Assert.AreEqual(5, River.CountAliveFishes(FishSizes, FishDirections));
+        }
+
+        [TestMethod]
+        public void Test1EatUpstreamers()
+        {
+            int fishWeight = 3;
+            Stack<int> upstreamers = new Stack<int>(new List<int> { 5, 2, 1 });
+
+            Assert.IsFalse(River.EatUpstreamers(fishWeight,ref upstreamers));
+            Assert.AreEqual(1, upstreamers.Count);
+        }
+        
+        [TestMethod]
+        public void Test2EatUpstreamers()
+        {
+            int fishWeight = 6;
+            Stack<int> upstreamers = new Stack<int>(new List<int> { 5, 2, 1 });
+
+            Assert.IsTrue(River.EatUpstreamers(fishWeight, ref upstreamers));
+            Assert.AreEqual(0, upstreamers.Count);
+        }
+        
+        [TestMethod]
+        public void Test3EatUpstreamers()
+        {
+            int fishWeight = 1;
+            Stack<int> upstreamers = new Stack<int>(new List<int> { 5, 2, 4 });
+
+            Assert.IsFalse(River.EatUpstreamers(fishWeight, ref upstreamers));
+            Assert.AreEqual(3, upstreamers.Count);
+        }
+    }
+}
